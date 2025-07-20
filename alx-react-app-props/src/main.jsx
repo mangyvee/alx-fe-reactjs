43d62062cd4fd76739c486a2eca81ed204663b2f
Vivian <makinivivian@gmail.com>
2025-07-20 20:57:39 +0300

    Moved UserProvider to main.jsx as required

--- alx-react-app-props/src/main.jsx
+++ alx-react-app-props/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { UserProvider } from './UserContext'; // Make sure the path is correct
+import { UserProvider } from './UserContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
