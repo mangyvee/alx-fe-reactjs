2b481c6b37c79f4e979f5fdb5976178e3b05e022
Vivian <makinivivian@gmail.com>
2025-07-20 19:53:40 +0300

    Fix UserContext implementation and remove prop drilling

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,8 +1,18 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
-const UserContext = createContext({
-  name: '',
-  email: ''
-});
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user] = useState({
+    name: 'John Doe',
+    email: 'johndoe@example.com'
+  });
+
+  return (
+    <UserContext.Provider value={user}>
+      {children}
+    </UserContext.Provider>
+  );
+};
 
 export default UserContext;
