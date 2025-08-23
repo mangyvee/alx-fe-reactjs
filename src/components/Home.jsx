6adcfb867aeb056cc9ade7333dbb6640d68b4424
Vivian <makinivivian@gmail.com>
2025-08-23 18:52:02 +0300

    Completed React Router advanced task with nested, dynamic, and protected routes; added styling

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the landing page of the React Router demo.</p>
+    </div>
+  );
+}
+
+export default Home;
